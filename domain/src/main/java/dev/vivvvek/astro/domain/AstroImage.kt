e790a7b67c2c581b96677143709d1f5770cd34a4
Dell <2307vivek@gmail.com>
2022-08-08 17:07:26 +0530

    created AstroImage.kt

---
+++ domain/src/main/java/dev/vivvvek/astro/domain/AstroImage.kt
@@ -0,0 +1,10 @@
+package dev.vivvvek.astro.domain
+
+data class AstroImage(
+    val title: String,
+    val copyright: String,
+    val date: String,
+    val explanation: String,
+    val url: String,
+    val hdUrl: String
+)
