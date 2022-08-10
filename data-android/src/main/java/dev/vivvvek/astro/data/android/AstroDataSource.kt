c0f3b28354a40f181331369a719c9a8c5a5a7597
Dell <2307vivek@gmail.com>
2022-08-10 14:56:37 +0530

    changed license

--- data-android/src/main/java/dev/vivvvek/astro/data/android/AstroDataSource.kt
+++ data-android/src/main/java/dev/vivvvek/astro/data/android/AstroDataSource.kt
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2021 Vivek Singh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.vivvvek.astro.data.android
 
 import com.google.gson.Gson
@@ -11,11 +26,11 @@ import java.io.InputStream
 
 class AstroDataSource(private val inputStream: InputStream) {
 
-    suspend fun getImages() : Response<List<AstroImage>> {
+    suspend fun getImages(): Response<List<AstroImage>> {
         return withContext(Dispatchers.IO) {
             try {
                 val jsonString = readJson(inputStream)
-                val imageType = object: TypeToken<List<AstroImage>>() {}.type
+                val imageType = object : TypeToken<List<AstroImage>>() {}.type
                 val images: List<AstroImage> = Gson().fromJson(jsonString, imageType)
                 Response.Success(images)
             } catch (e: IOException) {
@@ -24,7 +39,7 @@ class AstroDataSource(private val inputStream: InputStream) {
         }
     }
 
-    fun readJson(inputStream: InputStream) : String {
+    fun readJson(inputStream: InputStream): String {
         return inputStream
             .bufferedReader()
             .use { it.readText() }
--- data-android/src/test/java/dev/vivvvek/astro/data/android/AstroDataSourceTest.kt
+++ data-android/src/test/java/dev/vivvvek/astro/data/android/AstroDataSourceTest.kt
@@ -1,13 +1,27 @@
+/*
+ * Copyright 2021 Vivek Singh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package dev.vivvvek.astro.data.android
 
+import com.google.common.truth.Truth.assertThat
+import dev.vivvvek.astro.domain.Response
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import java.io.FileInputStream
 import java.io.InputStream
-import com.google.common.truth.Truth.assertThat
-import dev.vivvvek.astro.domain.Response
-import kotlinx.coroutines.runBlocking
-
 
 class AstroDataSourceTest {
 
@@ -41,11 +55,11 @@ class AstroDataSourceTest {
     }
 
     @Test
-    fun `check correct model deserialization, return_true` () = runBlocking {
+    fun `check correct model deserialization, return_true`() = runBlocking {
         val response = dataSource.getImages(inputStream)
         assertThat(response).isInstanceOf(Response.Success::class.java)
 
-        when(response) {
+        when (response) {
             is Response.Success -> {
                 assertThat(response.data[0].title).isEqualTo("Starburst Galaxy M94 from Hubble")
                 assertThat(response.data[1].title).isEqualTo("M27: The Dumbbell Nebula")
