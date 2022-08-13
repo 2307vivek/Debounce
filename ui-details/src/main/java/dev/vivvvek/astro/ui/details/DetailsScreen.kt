54d6d130a9352acbce199a1a21d806231bf2e1d3
Dell <2307vivek@gmail.com>
2022-08-13 14:21:17 +0530

    made DetailsScreen.kt

--- ui-details/src/main/java/dev/vivvvek/astro/ui/details/DetailsScreen.kt
+++ ui-details/src/main/java/dev/vivvvek/astro/ui/details/DetailsScreen.kt
@@ -1,23 +1,53 @@
 package dev.vivvvek.astro.ui.details
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import dev.vivvvek.astro.ui.home.AstroViewModel
 
+@OptIn(ExperimentalPagerApi::class)
 @Composable
 fun DetailsScreen(
     navController: NavController,
     viewModel: AstroViewModel
 ) {
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text(text = "DetailsScreen")
-        Button(onClick = { navController.popBackStack() }) {
-            Text(text = "Navigate back ${viewModel.selectedImageIndex}")
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .statusBarsPadding(),
+        topBar = {
+            ImageDetailTopAppBar { navController.navigateUp() }
+        }
+    ) {
+
+        val pagerState = rememberPagerState()
+
+        LaunchedEffect(viewModel.selectedImageIndex) {
+            pagerState.scrollToPage(viewModel.selectedImageIndex)
+        }
+        HorizontalPager(
+            state = pagerState,
+            count = viewModel.images.size,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(it)
+        ) { index ->
+            val image = remember(index) { viewModel.images[index] }
+            ImageDetails(
+                image = image,
+                modifier = Modifier.fillMaxSize()
+                    .padding(horizontal = 16.dp)
+            )
         }
     }
 }
---
+++ ui-details/src/main/java/dev/vivvvek/astro/ui/details/ImageDetails.kt
@@ -0,0 +1,107 @@
+package dev.vivvvek.astro.ui.details
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ContentAlpha
+import androidx.compose.material.LocalContentAlpha
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import dev.vivvvek.astro.domain.models.AstroImage
+import dev.vivvvek.astro.domain.models.Date
+import okhttp3.internal.trimSubstring
+
+@Composable
+fun ImageDetails(
+    modifier: Modifier = Modifier,
+    image: AstroImage
+) {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = modifier
+            .verticalScroll(
+                enabled = true,
+                state = scrollState
+            )
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(image.url)
+                .crossfade(true)
+                .build(),
+            contentDescription = image.title,
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.Center,
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color(220, 220, 220))
+                .aspectRatio(1f)
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        ImageDetailContent(image = image)
+    }
+}
+
+@Composable
+fun ImageDetailContent(
+    modifier: Modifier = Modifier,
+    image: AstroImage
+) {
+    Column(modifier = modifier) {
+        ImageNameAndDate(
+            name = image.title,
+            date = image.date,
+            copyright = image.copyright)
+        Spacer(modifier = Modifier.height(24.dp))
+
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text(
+                text = image.explanation,
+                style = MaterialTheme.typography.body1.copy(fontWeight = FontWeight.Normal),
+                lineHeight = 22.sp
+            )
+        }
+        Spacer(modifier = Modifier.height(72.dp))
+    }
+}
+
+@Composable
+fun ImageNameAndDate(
+    name: String,
+    date: Date,
+    copyright: String,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        Text(
+            text = name,
+            style = MaterialTheme.typography.h4
+        )
+        date.apply {
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(
+                    text = "${dayOfWeek.trimSubstring(0, 3)}, $monthName $day $year",
+                    style = MaterialTheme.typography.body2,
+                    fontWeight = FontWeight.Bold
+                )
+            }
+        }
+    }
+}
